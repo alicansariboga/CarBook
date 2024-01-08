90a6bb452c2b091fa4da636de8c6a339a14388f2
Ali Can <alicnn38@gmail.com>
2024-01-08 15:33:38 +0300

    last 3 blogs continued and some link are activated on navbar.

---
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_GetLast3BlogsWithAuthorListComponentPartial.cs
@@ -0,0 +1,33 @@
+﻿using CarBook.Dto.BlogDtos;
+using CarBook.Dto.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.BlogViewComponents
+{
+    public class _GetLast3BlogsWithAuthorListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _GetLast3BlogsWithAuthorListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        // getting veri. so it should be async
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //consume
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7210/api/Blogs/GetLast3BlogsWithAuthorsList");
+            if (responseMessage.IsSuccessStatusCode) //200 code
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLast3BlogsWithAuthors>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
