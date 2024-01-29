abb9af3529b1e0fd891d87c8b07d8ef36a7e1130
Ali Can <alicnn38@gmail.com>
2024-01-29 17:09:25 +0300

    Single blog parts.

--- Frontends/CarBook.WebUI/Controllers/BlogController.cs
+++ Frontends/CarBook.WebUI/Controllers/BlogController.cs
@@ -30,5 +30,11 @@ namespace CarBook.WebUI.Controllers
             }
             return View();
         }
+        public async Task<IActionResult> BlogDetail(int id)
+        {
+            ViewBag.v1 = "Bloglar";
+            ViewBag.v2 = "Blog Detayı ve Yorumlar";
+            return View();
+        }
     }
 }
