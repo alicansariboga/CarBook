eaf6ce4f721ab31c49fc3971767cb756638c6f69
Ali Can <alicnn38@gmail.com>
2024-01-30 16:40:04 +0300

    Fetching and listing the tags.

--- Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCloudTagByBlogComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCloudTagByBlogComponentPartial.cs
@@ -1,4 +1,5 @@
 ﻿using CarBook.Dto.BlogDtos;
+using CarBook.Dto.TagCloudDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -16,14 +17,14 @@ namespace CarBook.WebUI.ViewComponents.BlogViewComponents
         // getting veri. so it should be async
         public async Task<IViewComponentResult> InvokeAsync(int id)
         {
+            ViewBag.blogid = id;
             //consume
-
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:7210/api/Blogs/" + id);
+            var responseMessage = await client.GetAsync($"https://localhost:7210/api/TagClouds/GetTagCloudByBlogId?id=" + id);
             if (responseMessage.IsSuccessStatusCode) //200 code
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<GetBlogByIdDto>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<GetByBlogIdTagCloudDto>>(jsonData);
                 return View(values);
             }
             return View();
