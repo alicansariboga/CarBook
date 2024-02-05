81679f4981639cc650fbc8bed81dbbb2eebcec10
alicansariboga <alicnn38@gmail.com>
2024-02-05 14:34:41 +0300

    Banner page and routing operations in Area.

--- Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -1,5 +1,5 @@
 ﻿using CarBook.Dto.BannerDtos;
-using CarBook.Dto.BannerDtos;
+using CarBook.Dto.BrandDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Text;
@@ -7,6 +7,8 @@ using System.Text;
 namespace CarBook.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    // Banner routing
+    [Route("Admin/AdminBanner")]
     public class AdminBannerController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
@@ -16,6 +18,8 @@ namespace CarBook.WebUI.Areas.Admin.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
+        // Index routing in Admin/AdminBanner
+        [Route("Index")]
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
@@ -31,11 +35,13 @@ namespace CarBook.WebUI.Areas.Admin.Controllers
             return View();
         }
         [HttpGet]
+        [Route("CreateBanner")]
         public IActionResult CreateBanner()
         {
             return View();
         }
         [HttpPost]
+        [Route("CreateBanner")]
         public async Task<IActionResult> CreateBanner(CreateBannerDto createBannerDto)
         {
             var client = _httpClientFactory.CreateClient();
@@ -44,7 +50,46 @@ namespace CarBook.WebUI.Areas.Admin.Controllers
             var responseMessage = await client.PostAsync("https://localhost:7210/api/Banners/", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", "AdminBanner", new { area = "Admin" });
+            }
+            return View();
+        }
+        [Route("RemoveBanner/{id}")]
+        public async Task<IActionResult> RemoveBanner(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7210/api/Banners?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminBanner", new { area = "Admin" });
+            }
+            return View();
+        }
+        [HttpGet]
+        [Route("UpdateBanner/{id}")]
+        public async Task<IActionResult> UpdateBanner(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7210/api/Banners/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateBannerDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        [Route("UpdateBanner/{id}")] //Normally, there is no need to give the ID, but since the ID is expected during the route, I have to add it here as well.
+        public async Task<IActionResult> UpdateBanner(UpdateBannerDto updateBannerDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateBannerDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7210/api/Banners/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminBanner", new { area = "Admin" });
             }
             return View();
         }
