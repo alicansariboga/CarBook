3d8a8d57f2c9c414e526efed801251d1e2f58ada
alicansariboga <alicnn38@gmail.com>
2024-02-08 14:55:38 +0300

    RentACar entitiy was added successfully.

---
+++ Core/CarBook.Domain/Entities/RentACar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class RentACar
+    {
+        [Key]
+        public int RenACarID { get; set; }
+        public int PickUplocationID { get; set; }
+        [ForeignKey("PickUplocationID")]
+        public Location Location { get; set; }
+        public int CarID { get; set; }
+        [ForeignKey("CarID")]
+        public Car Car { get; set; }
+        public bool Available { get; set; }
+    }
+}
--- Infrastructure/CarBook.Persistence/Context/CarBookContext.cs
+++ Infrastructure/CarBook.Persistence/Context/CarBookContext.cs
@@ -34,5 +34,6 @@ namespace CarBook.Persistence.Context
         public DbSet<Author> Authors { get; set; }
         public DbSet<TagCloud> TagClouds { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<RentACar> RentACars { get; set; }
     }
 }
