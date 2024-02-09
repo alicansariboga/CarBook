0922fffed8250fcc46366d8ddda58d1ff9053da4
alicansariboga <alicnn38@gmail.com>
2024-02-09 14:56:12 +0300

    RentACarFilter

---
+++ Core/CarBoook.Application/Features/Mediator/Handlers/RentACarHandlers/GetRentACarQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using CarBoook.Application.Features.Mediator.Queries.RentACarQueries;
+using CarBoook.Application.Features.Mediator.Results.RentACarResults;
+using CarBoook.Application.Interfaces.RentACarInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBoook.Application.Features.Mediator.Handlers.RentACarHandlers
+{
+    public class GetRentACarQueryHandler : IRequestHandler<GetRentACarQuery, List<GetRentACarQueryResult>>
+    {
+        private readonly IRentACarRepository _repository;
+        public GetRentACarQueryHandler(IRentACarRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<GetRentACarQueryResult>> Handle(GetRentACarQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByFilterAsync(x => x.LocationID == request.LocationID && x.Available == true);
+            var results = values.Select(y => new GetRentACarQueryResult
+            {
+                CarId = y.CarID,
+            }).ToList();
+            return results;
+        }
+    }
+}
---
+++ Core/CarBoook.Application/Features/Mediator/Queries/RentACarQueries/GetRentACarQuery.cs
@@ -0,0 +1,17 @@
+﻿using CarBoook.Application.Features.Mediator.Results.RentACarResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBoook.Application.Features.Mediator.Queries.RentACarQueries
+{
+    public class GetRentACarQuery : IRequest<List<GetRentACarQueryResult>>
+    {
+        // for query in db
+        public int LocationID { get; set; }
+        public bool Available { get; set; }
+    }
+}
