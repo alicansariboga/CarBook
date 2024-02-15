9739515da5c718065a042d18fe5273a0de82a809
alicansariboga <alicnn38@gmail.com>
2024-02-15 15:45:23 +0300

    reservation page

--- Frontends/CarBook.WebUI/Controllers/ReservationController.cs
+++ Frontends/CarBook.WebUI/Controllers/ReservationController.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Controllers
 {
     public class ReservationController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ReservationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
         {
             ViewBag.v1 = "Araç Kiralama";
             ViewBag.v2 = "Araç Rezervasyon Formu";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7210/api/Locations/");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+            List<SelectListItem> values2 = (from x in values
+                                            select new SelectListItem
+                                            {
+                                                Text = x.Name,
+                                                Value = x.LocationID.ToString()
+                                            }).ToList();
+            ViewBag.v = values2;
             return View();
         }
     }
