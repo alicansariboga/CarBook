9f8cff7e1b866cd6c7cc5e1ea8272690d29c85d3
alicansariboga <alicnn38@gmail.com>
2024-02-26 12:54:27 +0300

    Car reservation page.

---
+++ Core/CarBoook.Application/Features/Mediator/Handlers/ReservationHandlers/CreateReservationCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using CarBook.Domain.Entities;
+using CarBoook.Application.Features.Mediator.Commands.ReservationCommands;
+using CarBoook.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBoook.Application.Features.Mediator.Handlers.ReservationHandlers
+{
+    public class CreateReservationCommandHandler : IRequestHandler<CreateReservationCommand>
+    {
+        private readonly IRepository<Reservation> _repository;
+
+        public CreateReservationCommandHandler(IRepository<Reservation> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateReservationCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new Reservation
+            {
+                Name = request.Name,
+                Surname = request.Surname,
+                Phone = request.Phone,
+                Email = request.Email,
+                CarID = request.CarID,
+                PickUpLocationID = request.PickUpLocationID,
+                DropOffLocationID = request.DropOffLocationID,
+                Age = request.Age,
+                LicenseYear = request.LicenseYear,
+                Description = request.Description,
+                Status = "Rezervasyon Alındı.",
+            });
+        }
+    }
+}
