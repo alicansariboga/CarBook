a4405dbc9a64e2c83ab3f89dd5287e79ea51b5c7
alicansariboga <alicnn38@gmail.com>
2024-02-27 15:34:13 +0300

    main page statistics.

--- Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
@@ -1,11 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.StatisticsDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Runtime.Intrinsics.Arm;
 
 namespace CarBook.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _DefaultStatisticsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            #region Statistic-carCount
+            var responseMessage1 = await client.GetAsync("https://localhost:7210/api/Statictics/GetCarCount/");
+            if (responseMessage1.IsSuccessStatusCode)
+            {
+                var jsonData1 = await responseMessage1.Content.ReadAsStringAsync();
+                var values1 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData1);
+                ViewBag.carCount = values1.carCount;
+            }
+            #endregion
+
+            #region Statistic-locationCount
+            var responseMessage2 = await client.GetAsync("https://localhost:7210/api/Statictics/GetLocationCount/");
+            if (responseMessage2.IsSuccessStatusCode)
+            {
+                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+                var values2 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData2);
+                ViewBag.locationCount = values2.locationCount;
+            }
+            #endregion
+
+            #region Statistic-brandCount
+            var responseMessage3 = await client.GetAsync("https://localhost:7210/api/Statictics/GetBrandCount/");
+            if (responseMessage3.IsSuccessStatusCode)
+            {
+                var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+                var values3 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData3);
+                ViewBag.brandCount = values3.brandCount;
+            }
+            #endregion
+
+            #region Statistic-carCountByFuelElectric
+            var responseMessage4 = await client.GetAsync("https://localhost:7210/api/Statictics/GetCarCountByFuelElectric/");
+            if (responseMessage4.IsSuccessStatusCode)
+            {
+                var jsonData4 = await responseMessage4.Content.ReadAsStringAsync();
+                var values4 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData4);
+                ViewBag.carCountByFuelElectric = values4.carCountByFuelElectric;
+            }
+            #endregion
+
             return View();
         }
     }
