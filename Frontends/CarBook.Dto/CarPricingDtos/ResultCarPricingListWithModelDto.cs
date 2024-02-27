e422000097de271a4a3f53c9955876177838039d
alicansariboga <alicnn38@gmail.com>
2024-02-27 14:10:01 +0300

    A discount was applied to the vehicle price packages, but the transactions could not be fully completed by the controller. It will be reviewed later.

--- Frontends/CarBook.Dto/CarPricingDtos/ResultCarPricingListWithModelDto.cs
+++ Frontends/CarBook.Dto/CarPricingDtos/ResultCarPricingListWithModelDto.cs
@@ -8,7 +8,7 @@ namespace CarBook.Dto.CarPricingDtos
 {
 	public class ResultCarPricingListWithModelDto
 	{
-		public string Brand { get; set; }
+        public string Brand { get; set; }
 		public string model { get; set; }
 		public string CoverImageUrl { get; set; }
 		public decimal dailyAmount { get; set; }
--- Frontends/CarBook.WebUI/Controllers/CarPricingController.cs
+++ Frontends/CarBook.WebUI/Controllers/CarPricingController.cs
@@ -1,6 +1,7 @@
 ﻿using CarBook.Dto.CarPricingDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using NPoco.Linq;
 
 namespace CarBook.WebUI.Controllers
 {
@@ -17,15 +18,32 @@ namespace CarBook.WebUI.Controllers
             ViewBag.v1 = "Paketler";
             ViewBag.v2 = "Araç Fiyat Paketleri";
 
-			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync("https://localhost:7210/api/CarPricings/GetCarPricingWithTimePeriodList");
-			if (responseMessage.IsSuccessStatusCode) //200 code
-			{
-				var jsonData = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<List<ResultCarPricingListWithModelDto>>(jsonData);
-				return View(values);
-			}
-			return View();
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7210/api/CarPricings/GetCarPricingWithTimePeriodList");
+            if (responseMessage.IsSuccessStatusCode) //200 code
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarPricingListWithModelDto>>(jsonData);
+
+                var priceChanges = new Dictionary<int, (decimal weeklyAmount, decimal monthlyAmount)>();
+
+                var index = -1;
+                for (int i = 0; i < values.Count; i++)
+                {
+                    index++;
+                    var prices = values[i];
+                    decimal weeklyAmount1 = prices.dailyAmount * 7;
+                    decimal monthlyAmount1 = prices.weeklyAmount * 4;
+
+                    priceChanges.Add(i, (weeklyAmount1, monthlyAmount1));
+                    ViewBag.i = index;
+                }
+
+                ViewBag.PriceChanges = priceChanges;
+
+                return View(values);
+            }
+            return View();
         }
     }
 }
