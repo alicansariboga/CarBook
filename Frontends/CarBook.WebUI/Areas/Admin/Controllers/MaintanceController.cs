b7585ae96911eb3a919285a9933dd5830488fe20
alicansariboga <alicnn38@gmail.com>
2024-03-04 13:51:04 +0300

    Maintance.

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/MaintanceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    public class MaintanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
