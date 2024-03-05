5769ee64f9781e384ef79c8d7820805c078243ed
alicansariboga <alicnn38@gmail.com>
2024-03-05 15:18:01 +0300

    Car detail page. An error detail page about the list.

--- Frontends/CarBook.WebUI/Controllers/CarController.cs
+++ Frontends/CarBook.WebUI/Controllers/CarController.cs
@@ -1,5 +1,4 @@
-﻿using CarBook.Dto.CarDtos;
-using CarBook.Dto.CarPricingDtos;
+﻿using CarBook.Dto.CarPricingDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -28,6 +27,13 @@ namespace CarBook.WebUI.Controllers
                 var values = JsonConvert.DeserializeObject<List<ResultCarPricingWithCarDto>>(jsonData);
                 return View(values);
             }
+            return View();
+        }
+        public async Task<IActionResult> CarDetail(int id)
+        {
+            ViewBag.v1 = "Araç Detayları";
+            ViewBag.v2 = "Araca Ait Teknik Özellik Ve Aksesuarlar";
+            ViewBag.carid = id;
             return View();
         }
     }
---
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailMainCarFeatureComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
+{
+	public class _CarDetailMainCarFeatureComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _CarDetailMainCarFeatureComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		public async Task<IViewComponentResult> InvokeAsync(int id)
+		{
+			ViewBag.carid = id;
+
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync($"https://localhost:7210/api/Cars/{id}");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<ResultCarWithBrandsDto>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
+}
