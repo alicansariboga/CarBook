ffabe764ebc52cf3c0d82dc2b4f4acfa096bba8d
alicansariboga <alicnn38@gmail.com>
2024-03-06 15:44:21 +0300

    Review comment page is completed.

--- Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailCommentsByCarIdComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailCommentsByCarIdComponentPartial.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.ReviewDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
 {
     public class _CarDetailCommentsByCarIdComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
-        {
-            return View();
-        }
-    }
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _CarDetailCommentsByCarIdComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(int id)
+		{
+			ViewBag.carid = id;
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7210/api/Reviews?id=" + id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultReviewByCarIdDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
 }
