989adba8ab8ce47049611fd94f3490393058558d
alicansariboga <alicnn38@gmail.com>
2024-03-07 13:40:16 +0300

    appUser and App Role entities.

--- Infrastructure/CarBook.Persistence/Migrations/20240307103352_mig_add_AppUser_App_Role.Designer.cs
+++ Infrastructure/CarBook.Persistence/Migrations/20240307103352_mig_add_AppUser_App_Role.Designer.cs
@@ -4,6 +4,7 @@ using CarBook.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarBook.Persistence.Migrations
 {
     [DbContext(typeof(CarBookContext))]
-    partial class CarBookContextModelSnapshot : ModelSnapshot
+    [Migration("20240307103352_mig_add_AppUser_App_Role")]
+    partial class mig_add_AppUser_App_Role
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
