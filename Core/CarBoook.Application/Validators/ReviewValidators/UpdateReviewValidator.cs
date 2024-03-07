6c4254f83002e88a43cb92c81dde34ecf6cc1e76
alicansariboga <alicnn38@gmail.com>
2024-03-07 11:00:12 +0300

    Update Review Validation.

---
+++ Core/CarBoook.Application/Validators/ReviewValidators/UpdateReviewValidator.cs
@@ -0,0 +1,25 @@
+﻿using CarBoook.Application.Features.Mediator.Commands.ReviewCommands;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBoook.Application.Validators.ReviewValidators
+{
+	public class UpdateReviewValidator : AbstractValidator<UpdateReviewCommand>
+	{
+		public UpdateReviewValidator()
+		{
+			RuleFor(x => x.CustomerName).NotEmpty().WithMessage("Bu alan boş bırakılamaz.");
+			RuleFor(x => x.CustomerName).MinimumLength(5).WithMessage("En az 5 karakter olmalıdır.");
+			RuleFor(x => x.RatingValue).NotEmpty().WithMessage("Bu alan boş bırakılamaz.");
+			RuleFor(x => x.Comment).NotEmpty().WithMessage("Bu alan boş bırakılamaz.");
+			RuleFor(x => x.Comment).MinimumLength(50).WithMessage("En az 50 karakter olmalıdır.");
+			RuleFor(x => x.Comment).MaximumLength(500).WithMessage("En fazla 500 karakter olmalıdır.");
+			RuleFor(x => x.CustomerImage).NotEmpty().WithMessage("Bu görsel URL adresini giriniz.").MinimumLength(10).WithMessage("En az 10 karakter uzunluğunda olmalıdır.").MaximumLength(200).WithMessage("En fazla 200 karakter uzunluğunda olmalıdır.") ;
+
+		}
+	}
+}
