25a7e4d40999e713c6ae36958a3c01b6477e13fc
alicansariboga <alicnn38@gmail.com>
2024-03-07 15:05:20 +0300

    Create JWT.

---
+++ Core/CarBoook.Application/Tools/JwtTokenGenerator.cs
@@ -0,0 +1,40 @@
+﻿using CarBoook.Application.Dtos;
+using CarBoook.Application.Features.Mediator.Results.AppUserResults;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBoook.Application.Tools
+{
+    public class JwtTokenGenerator
+    {
+        public static TokenResponseDto GenerateToken(GetCheckAppUserQueryResult result)
+        {
+            var claims = new List<Claim>();
+            if (!string.IsNullOrWhiteSpace(result.Role))
+            {
+                claims.Add(new Claim(ClaimTypes.Role, result.Role)); //new client
+            }
+            claims.Add(new Claim(ClaimTypes.NameIdentifier,result.AppUserId.ToString());
+
+            if(!string.IsNullOrWhiteSpace(result.Username))
+            {
+                claims.Add(new Claim("Username",result.Username));
+            }
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtTokenDefaults.Key));
+            var signingCredentials=new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+            var expireDate = DateTime.UtcNow.AddDays(JwtTokenDefaults.Expire);
+
+            // identify token
+            JwtSecurityToken token = new JwtSecurityToken(issuer: JwtTokenDefaults.ValidIssuer, audience: JwtTokenDefaults.ValidAudience, claims: claims, notBefore: DateTime.UtcNow, expires: expireDate, signingCredentials: signingCredentials);
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            return new TokenResponseDto(tokenHandler.WriteToken(token), expireDate);
+        }
+    }
+}
