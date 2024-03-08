1cd1dcf4a2a1d7532d066ac057f2c70c9ede6d1e
alicansariboga <alicnn38@gmail.com>
2024-03-08 10:29:54 +0300

    Random Key is added.

--- Core/CarBoook.Application/Tools/JwtTokenDefaults.cs
+++ Core/CarBoook.Application/Tools/JwtTokenDefaults.cs
@@ -10,7 +10,7 @@ namespace CarBoook.Application.Tools
     {
         public const string ValidAudience = "https://localhost";
         public const string ValidIssuer = "https://localhost";
-        public const string Key = "1CAR18B74OO51K34PRO9J1E3K3T828T4E5S9T30"; // Ranodom : "A34F86311146B26952AF4A332A12A"
+        public const string Key = "1CAR18B74OO51K34PRO9J1E3K3T828T4E5S9T30"; // Random : "A34F86311146B26952AF4A332A12A"
         public const int Expire = 3; // min
     }
 }
