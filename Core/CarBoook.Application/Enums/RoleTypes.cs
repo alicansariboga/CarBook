9a34f589b235fb4da368b50eb418c8c4274def87
alicansariboga <alicnn38@gmail.com>
2024-03-08 10:39:09 +0300

    Create signup command and enum for role types.

---
+++ Core/CarBoook.Application/Enums/RoleTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBoook.Application.Enums
+{
+    public enum RoleTypes
+    {
+        Admin = 1,
+        Manager = 2,
+        Member = 3,
+        Visitor = 4
+    }
+}
---
+++ Core/CarBoook.Application/Features/Mediator/Handlers/AppUserHandlers/CreateAppUserCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using CarBook.Domain.Entities;
+using CarBoook.Application.Enums;
+using CarBoook.Application.Features.Mediator.Commands.AppUserCommands;
+using CarBoook.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBoook.Application.Features.Mediator.Handlers.AppUserHandlers
+{
+    public class CreateAppUserCommandHandler : IRequestHandler<CreateAppUserCommand>
+    {
+        private readonly IRepository<AppUser> _repository;
+
+        public CreateAppUserCommandHandler(IRepository<AppUser> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAppUserCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new AppUser
+            {
+                UserName = request.Username,
+                Password = request.Password,
+                // AppRoleID= 3, // Default
+                AppRoleID = (int)RoleTypes.Member, // Enum
+            });
+        }
+    }
+}
