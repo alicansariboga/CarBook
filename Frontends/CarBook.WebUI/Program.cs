a32d9202b01d21790180172956815bc5c57742cf
alicansariboga <alicnn38@gmail.com>
2024-03-11 14:40:50 +0300

    css.

--- Frontends/CarBook.WebUI/Program.cs
+++ Frontends/CarBook.WebUI/Program.cs
@@ -39,7 +39,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Default}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.UseEndpoints(endpoints =>
 {
